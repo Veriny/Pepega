80faf4fb12b6f11a4632f657b93734ebd075babd
Justin Im <20imj68@myahs.org>
2019-07-27 15:57:49 -0700

    Full code finished

--- pepega/pepega.ino
+++ pepega/pepega.ino
@@ -1,9 +1,81 @@
+#include "Seeed_BMP280.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+File fortnite;
+BMP280 minecraft;
+
+
 void setup() {
   // put your setup code here, to run once:
-
-}
+  Serial.begin(9600);
+  
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+     Serial.print("Initializing SD card...");
+    if(!minecraft.init()) {
+        Serial.println("Device not connected or broken!");
+     }
+    if (!SD.begin(4)) {
+      Serial.println("initialization failed!");
+      while (1);
+    }
+    fortnite = SD.open("fortnite.txt", FILE_WRITE);
+    if (fortnite) {
+      Serial.print("Writing to test.txt...");
+      fortnite.println("Fortnite bad, minecraft good.");
+      // close the file:
+      fortnite.close();
+      Serial.println("done.");
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+    }
+    
+ }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(5000);
+  write();
+  
+}
 
+//void wait(int seconds){
+//  clock_t endwait;
+//  endwait=clock()+seconds*CLOCKS_PER_SEC;
+//  while (clock()<endwait);
+//}
+
+void write(){
+  Serial.begin(9600);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+     Serial.print("Pepega Clap");
+
+    if (!SD.begin(4)) {
+      Serial.println("initialization failed!");
+      while (1);
+    }
+    fortnite = SD.open("fortnite.txt", FILE_WRITE);
+    if (fortnite) {
+      Serial.print("Writing to test.txt...");
+      Serial.print(minecraft.getPressure());
+      fortnite.println("Fortnite bad, minecraft good.");
+      fortnite.println("Pressure:");
+      fortnite.println(minecraft.getPressure());
+      fortnite.println("Temp:");
+      fortnite.println(minecraft.getTemperature());
+      fortnite.println("Altitude:");
+      fortnite.println(minecraft.calcAltitude(minecraft.getPressure()));
+      // close the file:
+      fortnite.close();
+      Serial.println("done.");
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+    }
 }
